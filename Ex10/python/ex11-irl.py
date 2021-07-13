ee4e496f27e310a9258ee26c5a26d7c4a689ede6
kaibs <kai.b.schneider@gmail.com>
2021-07-13 06:42:58 +0200

    Ex11 1a (+ beginning b)

--- Ex10/python/ex11-irl.py
+++ Ex10/python/ex11-irl.py
@@ -68,17 +68,136 @@ def value_iteration(env, rewards):
     return policy
 
 
+# calc pol by choosing most occuring a f.e. s 
+def get_naive_pol(env, trajs):
+
+    n_states = env.observation_space.n
+    n_actions = env.action_space.n
+
+    pairs = {}
+
+    # count pairs
+    for traj in trajs:
+        for pair in traj:
+
+            if pair in pairs:
+                pairs[pair] += 1
+            
+            else:
+                pairs[pair] = 1
+
+
+    max_pairs = {}
+
+    # get highest scoring a for each s
+    for s in range(n_states):
+
+        max_a = 0
+        score = 0
+
+        for a in range(n_actions):
+
+            if (s,a) in pairs:
+                if pairs[(s,a)] > score:
+                    max_a = a
+
+        max_pairs[s] = max_a
+
+    return max_pairs
+
+
+
+# get probabilities for p(s'|s,a) from experience
+def get_state_probs(env, trajs):
+
+    n_states = env.observation_space.n
+    n_actions = env.action_space.n
+
+    # counts(s,a,s')
+    counts = np.zeros((n_states, n_actions, n_states))
+
+    for traj in trajs:
+        for i in range(len(traj)-1):
+            
+            s_new = traj[i+1][0]
+            s = traj[i][0]
+            a = traj[i][1]
+
+            counts[s][a][s_new] += 1
+
+
+    probs = np.zeros((n_states, n_actions, n_states))
+
+    # calc prob for each s' for each (s,a)
+    for s in range(n_states):
+        for a in range(n_actions):
+
+            total_states = int(sum(counts[s][a]))
+
+            for s_new in range(n_actions):
+
+                if total_states != 0:
+                    probs[s][a][s_new] = counts[s][a][s_new]/total_states
+    
+
+    return probs
+
+
+# calc state frequencies
+def get_state_freqs(env, trajs, mi, pol, t):
+
+    n_states = env.observation_space.n
+    n_actions = env.action_space.n
+
+    mi_new = np.ones(n_states)
+
+    probs = get_state_probs(env, trajs)
+
+    for s_n in range(n_states):
+        new_val = 0
+
+        for s in range(n_states):
+
+            for a in range(n_actions):
+
+                new_val = probs[s][a][s_n]*mi[s]*pol((s,a))
+
+
+        mi_new[s_n] = new_val
+
+
+    #s_freq = (1/T) * 
+    
+    #return s_freqs
+    return 0
+
 
 
 def main():
     env = gym.make('FrozenLake-v0')
-    #env.render()
+    env.render()
+    print("")
     env.seed(0)
     np.random.seed(0)
     expertpolicy = [0, 3, 0, 3, 0, 0, 0, 0, 3, 1, 0, 0, 0, 2, 1, 0]
-    trajs = generate_demonstrations(env, expertpolicy, 0.1, 20)  # list of trajectories
+    trajs = generate_demonstrations(env, expertpolicy, 0.1, 100)  # list of trajectories
     print("one trajectory is a list with (state, action) pairs:")
     print (trajs[0])
+
+    ### (a) naive policy
+    max_pairs = get_naive_pol(env, trajs)
+    naive_pol = list(max_pairs.values())
+
+    print("naive:  " + str(naive_pol))
+
+
+    ### (b) state visiting freqs
+
+    #probs = get_state_probs(env, trajs)
+
+
+    # get pol
+    #opt_pol = value_iteration(env, rewards)
 
 
 
